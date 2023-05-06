b5ccadae407823f2034e88d428648835b36d35e8
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-06 23:20:51 +0700

    (Not Succes)Numbers type of data in JS

---
+++ Numbers.js
@@ -0,0 +1,11 @@
+// numbers
+
+// Penjelasan:
+// Di dalam javascript 
+//   - Memiliki Satu jenis 
+//   - Bilangan Positif 
+//   - Bil Negatif 
+//   - Seluruh angka (Int)
+//   - Angka Desimal 
+
+3 + 3 
